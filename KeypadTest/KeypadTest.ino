c7449b55ab2c2aacd4056195739a666ab82f37ab
Mårten Åsberg <marten.asberg@outlook.com>
2024-07-06 12:02:32 +0200

    KeypadTest, example code

---
+++ KeypadTest/KeypadTest.ino
@@ -0,0 +1,26 @@
+#include <Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 3;
+char keys[ROWS][COLS] = {
+  { '1', '2', '3' },
+  { '4', '5', '6' },
+  { '7', '8', '9' },
+  { '*', '0', '#' }
+};
+byte rowPins[ROWS] = { 19, 14, 15, 17 };
+byte colPins[COLS] = { 18, 20, 16 };
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char key = keypad.getKey();
+
+  if (key) {
+    Serial.println(key);
+  }
+}
